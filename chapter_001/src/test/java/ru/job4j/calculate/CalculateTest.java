e614b0fe5cf844a1d40e519f5341d9595da97816
VooltMace <mazanblu@mail.ru>
2019-06-24 13:01:13 +0300

    Использование Git в IDEA[#148569]

--- chapter_001/src/test/java/ru/job4j/calculate/CalculateTest.java
+++ chapter_001/src/test/java/ru/job4j/calculate/CalculateTest.java
@@ -12,17 +12,17 @@ import static org.junit.Assert.assertThat;
  * @since 0.1
  */
 public class CalculateTest {
- /**
- * Test echo.
- */
+    /**
+     * Test echo.
+     */
 
-@Test
- public void whenTakeNameThenThreeEchoPlusName() {
-    String input = "job4j";
-    String expect = "Echo, echo, echo : job4j"; 
-    Calculate calc = new Calculate();
-    String result = calc.echo(input);
-    assertThat(result, is(expect));
- }
+    @Test
+    public void whenTakeNameThenThreeEchoPlusName() {
+        String input = "job4j";
+        String expect = "Echo, echo, echo : job4j";
+        Calculate calc = new Calculate();
+        String result = calc.echo(input);
+        assertThat(result, is(expect));
+    }
 
 }
