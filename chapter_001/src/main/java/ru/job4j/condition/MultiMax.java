def2d2a270daf14fd0f385c91c387b29550723ce
VooltMace <mazanblu@mail.ru>
2019-07-16 13:38:39 +0300

    4.3. Вычисление площади треугольника[#148582]

---
+++ chapter_001/src/main/java/ru/job4j/condition/MultiMax.java
@@ -0,0 +1,16 @@
+package ru.job4j.condition;
+
+public class MultiMax {
+    public int max(int first, int second, int third) {
+        int result;
+        // if ...
+        if (second > first) {
+            result = second;
+        } else result = first;
+        if (second > third) {
+            result = second;
+        } else result = third;
+
+        return result;
+    }
+}
