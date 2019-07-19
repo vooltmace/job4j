e63ca8d18181cca01b749a1d71b092c9bddfe6e9
VooltMace <mazanblu@mail.ru>
2019-07-19 21:01:10 +0300

    Максимум из трех чисел[#148580]Fix3

--- chapter_001/src/main/java/ru/job4j/condition/MultiMax.java
+++ chapter_001/src/main/java/ru/job4j/condition/MultiMax.java
@@ -4,30 +4,14 @@ public class MultiMax {
     public int max(int first, int second, int third) {
         int result;
         // if ....
-        if (second > first) {
-            result = second;
-        } else result = first;
-
-        if (second > third) {
-            result = second;
-        } else result = third;
-
         if (first > second) {
             result = first;
-        } else result = second;
-
-        if (first > third) {
-            result = first;
-        } else result = third;
-
-        if (third > first) {
+        } else {
+            result = second;
+        }
+        if (result < third) {
             result = third;
-        } else result = first;
-
-        if (third > second) {
-            result = third;
-        } else result = second;
-
+        }
         return result;
     }
 }
