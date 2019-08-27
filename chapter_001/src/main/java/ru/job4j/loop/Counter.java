5c7f7553705d0c498603279fc4fc6b4c53857442
VooltMace <mazanblu@mail.ru>
2019-08-27 23:25:22 +0300

    5.1. Подсчет суммы чётных чисел в диапазоне[#148587]Fix4

--- chapter_001/src/main/java/ru/job4j/loop/Counter.java
+++ chapter_001/src/main/java/ru/job4j/loop/Counter.java
@@ -4,9 +4,9 @@ public class Counter {
     public static int sum(int start, int finish) {
         int result = 0;
 
-        for (int index = start; index <= finish; index += 2) {
+        for (int index = start; index <= finish; index ++)
+        if(index % 2 == 0)
             result = result + index;
-        }
         return result;
     }
 }
